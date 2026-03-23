167b85c5b86fb90dec4c11423b2b7b6a0b5e3383
sebastianbodor <sebastianbodor@gmail.com>
2026-03-23 10:00:10 +0100

    Verzia pre deploy
    Testovanie deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+//import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage';
 import EditPage from './pages/EditPage';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/EditPage" element={<EditPage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { powerApps } from "@microsoft/power-apps-vite/plugin"
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), powerApps()],
+  build: {
+    rollupOptions: {
+      output: {
+        format: "iife",  // toto je kľúčové
+        inlineDynamicImports: true,
+      }
+    }
+  }
 });
